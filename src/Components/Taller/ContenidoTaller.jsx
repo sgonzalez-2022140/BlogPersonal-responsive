54483a5ea0957fcb812e87ca6142b25e2bd52ee5
sgonzalez-2022140 <sgonzalez-2022140@kinal.edu.gt>
2024-05-10 08:29:21 -0600

    Texto y diseño basico de pestaña taller

--- src/Components/Taller/ContenidoTaller.jsx
+++ src/Components/Taller/ContenidoTaller.jsx
@@ -1,7 +1,25 @@
+import styled from "styled-components"
 
+const StyledTaller = styled.div`
+    text-align: center;
+    
+    h1{
+        font-family: Helvetica;
+        font-weight: 600;
+        color: #07326b
+    } 
+    span{
+            color: #17252a;
+        }
 
+    
+`
 export const ContenidoTaller = () => {
   return (
-    <div>ContenidoTaller</div>
+    <>
+        <StyledTaller>
+            <h1>TALLER DE <span>INFORMÁTICA</span></h1>
+        </StyledTaller>
+    </>
   )
 }
--- src/Pages/Taller.jsx
+++ src/Pages/Taller.jsx
@@ -2,7 +2,9 @@ import { ContenidoTaller } from "../Components/Taller/ContenidoTaller"
 import styled from "styled-components"
 
 const StyledPage = styled.div`
-    
+    background-color: #DEF2F1;
+
+
 `
 
 export const Taller = () => {
