26d8df7dba8294feed696809453b2800dce2011f
sgonzalez-2022140 <sgonzalez-2022140@kinal.edu.gt>
2024-05-10 17:08:02 -0600

    Pagina de taller terminada

--- src/Components/Taller/ContenidoTaller.jsx
+++ src/Components/Taller/ContenidoTaller.jsx
@@ -6,6 +6,9 @@ import taller1 from "../../img/taller1.png"
 import adopcion from "../../img/mascotas.png"
 import empresas from "../../img/empresastaller.png"
 import final1 from "../../img/final1.png"
+import final from "../../img/proyectoFinal.png"
+import hacker from "../../img/hackerrank.png"
+import hoteleria from "../../img/hoteleria.png"
 
 const StyledTaller = styled.div`
     text-align: center;
@@ -48,6 +51,9 @@ export const ContenidoTaller = () => {
     const tallerAdo = adopcion;
     const empresas2 = empresas;
     const final1taller = final1;
+    const finaltaller = final
+    const hackerank = hacker
+    const hotel = hoteleria
 
     return (
         <StyledTaller>
@@ -58,7 +64,7 @@ export const ContenidoTaller = () => {
                     <Col lg={6} className="mb-4">
                         <Card className="shadow p-3 mb-5 bg-body rounded carta">
                             <Card.Body>
-                                <h1 className="titulo-act">Laboratorio #1</h1>
+                                <h1 className="titulo-act">Laboratorio #1 (1er Bim)</h1>
                                 <h3>Descripción de la actividad</h3>
                                 <p className="contenido">Creación de una página web sobre nuestra vida y luego debíamos realizar un nuevo diseño de la página del colegio.</p>
                                 <br />
@@ -72,7 +78,7 @@ export const ContenidoTaller = () => {
                     <Col lg={6} className="mb-4">
                         <Card className="shadow p-3 mb-5 bg-body rounded carta">
                             <Card.Body>
-                                <h1 className="titulo-act">Laboratorio #2</h1>
+                                <h1 className="titulo-act">Laboratorio #2 (1er Bim)</h1>
                                 <h3>Descripción de la actividad</h3>
                                 <p className="contenido">Creación de backend de adopción de mascotas con validaciones y métodos especificos hechos en express</p>
                                 <br />
@@ -89,7 +95,7 @@ export const ContenidoTaller = () => {
                     <Col lg={6} className="mb-4">
                         <Card className="shadow p-3 mb-5 bg-body rounded carta">
                             <Card.Body>
-                                <h1 className="titulo-act">Laboratorio #3</h1>
+                                <h1 className="titulo-act">Laboratorio #3 (1er Bim)</h1>
                                 <h3>Descripción de la actividad</h3>
                                 <p className="contenido">Creación de un gestor de empresas con métodos get, post, update y delete en Express (junto a sus validaciones) </p>
                                 <br />
@@ -103,7 +109,7 @@ export const ContenidoTaller = () => {
                     <Col lg={6} className="mb-4">
                         <Card className="shadow p-3 mb-5 bg-body rounded carta">
                             <Card.Body>
-                                <h1 className="titulo-act">Avances de proyecto final</h1>
+                                <h1 className="titulo-act">Avances de proyecto final (1er Bim)</h1>
                                 <h3>Descripción de la actividad</h3>
                                 <p className="contenido">Revisión del 30% backend del proyecto final de bimestre con valor total de 50pts</p>
                                 <br />
@@ -113,6 +119,54 @@ export const ContenidoTaller = () => {
                             </Card.Body>
                         </Card>
                     </Col>
+                </Row>
+            </Container>
+            <Container fluid style={{ width: "70%" }}>
+                <Row className="justify-content-center">
+                    <Col lg={6} className="mb-4">
+                        <Card className="shadow p-3 mb-5 bg-body rounded carta">
+                            <Card.Body>
+                                <h1 className="titulo-act">Proyecto Final (1er Bim)</h1>
+                                <h3>Descripción de la actividad</h3>
+                                <p className="contenido">Elaboración del backend de una empresa con todos los conocimientos obtenidos en clases. </p>
+                                <br />
+                                <h3>Habilidades:</h3>
+                                <p className="contenido">Manejo de datos no relacionales, uso de tokens, Paquetes de Node.js y Express</p>
+                                <center><Card.Img variant="top" src={finaltaller} alt="Santiago" className="imagen" /></center>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    
+                    <Col lg={6} className="mb-4">
+                        <Card className="shadow p-3 mb-5 bg-body rounded carta">
+                            <Card.Body>
+                                <h1 className="titulo-act">Ejercicios de HackerRank (2do Bim)</h1>
+                                <h3>Descripción de la actividad</h3>
+                                <p className="contenido">Elaboración de ejercicios fáciles, medio y difíciles de codigo en una plataforma llamada HackeRank</p>
+                                <br />
+                                <h3>Habilidades:</h3>
+                                <p className="contenido">Pensamiento analítico y Capacidad de resolución de problemas </p>
+                                <center><Card.Img variant="top" src={hackerank} alt="Santiago" className="imagen" /></center>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+            <Container fluid style={{ width: "70%" }}>
+                <Row className="justify-content-center">
+                    <Col lg={6} className="mb-4">
+                        <Card className="shadow p-3 mb-5 bg-body rounded carta">
+                            <Card.Body>
+                                <h1 className="titulo-act">Avances (2do Bim)</h1>
+                                <h3>Descripción de la actividad</h3>
+                                <p className="contenido">Elaboración del proyecto de hoteleria en un 30% con Frontend y backend </p>
+                                <br />
+                                <h3>Habilidades:</h3>
+                                <p className="contenido">Aplicación del modelo MERN Stack</p>
+                                <center><Card.Img variant="top" src={hotel} alt="Santiago" className="imagen" /></center>
+                            </Card.Body>
+                        </Card>
+                    </Col>                                        
                 </Row>
             </Container>
         </StyledTaller>
