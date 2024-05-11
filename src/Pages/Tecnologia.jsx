6c156b0f33241bc352d3537d92d01b5cfefb6374
sgonzalez-2022140 <sgonzalez-2022140@kinal.edu.gt>
2024-05-10 19:19:32 -0600

    Solo falta agregar link en las imagenes

--- src/Pages/Tecnologia.jsx
+++ src/Pages/Tecnologia.jsx
@@ -1,9 +1,21 @@
 import { TecnologiaComp } from "../Components/Tecnologia/TecnologiaComp"
+import styled from "styled-components"
+
+
+const StyledPage = styled.div`
+    background-color: #DEF2F1;
+`
+
+
 
 export const Tecnologia = () => {
+
+ 
+
   return (
-    <>
-        <TecnologiaComp/>
-    </>
+    <StyledPage>
+    <TecnologiaComp/>
+    
+    </StyledPage>
   )
 }
